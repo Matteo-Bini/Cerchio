a1760acb0c65b0617143c6db0bff1e5073f07ab5
matte <matte@DESKTOP-NA3JRQD>
2024-01-28 11:15:42 +0100

    Aggiungere i file di progetto.

---
+++ Cerchio/Cerchio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerchio
+{
+    internal class Cerchio
+    {
+        public int Raggio { get; set; }
+        public int Perimetro()
+        {
+            return (int)(Raggio*2*Math.PI);
+        }
+        public int Area()
+        {
+            return (int)(Raggio*Raggio*Math.PI);
+        }
+        public override string ToString()
+        {
+            return string.Format("Cerchio R = {0}", Raggio);
+        }
+    }
+}
---
+++ Cerchio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerchio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio c = new Cerchio();
+            c.Raggio = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} \n{1}", c.Perimetro(), c.Area());
+            Console.WriteLine("{0}", c.ToString());
+            Console.ReadLine();
+        }
+    }
+}
